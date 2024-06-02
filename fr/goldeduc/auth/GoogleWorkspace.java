d9fa6da1d95a096a64c13216971bc653bf610ac8
Matteo C <hello@micorksen.eu>
2024-06-02 20:02:01 +0200

    Create GoogleWorkspace.java

---
+++ fr/goldeduc/auth/GoogleWorkspace.java
@@ -0,0 +1,41 @@
+package fr.goldeduc.auth;
+
+import fr.wseduc.webutils.Either;
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.entcore.auth.services.impl.AbstractSSOProvider;
+import org.entcore.common.neo4j.Neo4j;
+import org.entcore.common.neo4j.Neo4jResult;
+import org.opensaml.saml2.core.Assertion;
+
+public class GoogleWorkspace extends AbstractSSOProvider {
+  @Override
+  public void execute(Assertion assertion, Handler<Either<String, Object>> handler) {
+    handler.handle(new Either.Left<String, Object>("Execute function isn't available on this implementation."));
+  }
+
+  @Override
+  public void generate(EventBus eb, String userId, String host, String serviceProviderEntityId, Handler<Either<String, JsonArray>> handler) {
+    String query = "MATCH (u:User {id:{userId}}) RETURN u.profiles as profiles, u.email as email";
+    Neo4j.getInstance().execute(query, new JsonObject().put("userId", userId), Neo4jResult.validUniqueResultHandler(evt -> {
+      if (evt.isLeft()) {
+        handler.handle(new Either.Left(evt.left().getValue()));
+        return;
+      }
+
+      JsonArray result = new JsonArray();
+      JsonObject user = evt.right().getValue();
+      JsonObject profiles = user.getJsonArray("profiles");
+      
+      if (!profiles.contains("Personnel") && !profiles.contains("Teacher")) {
+        handler.handle(new Either.Left<String, Object>("invalid.user.profile"));
+        return;
+      }
+      
+      result.add(new JsonObject().put("email", user.getString("email", "")));
+      handler.handle(new Either.Right<>(result));
+    }));
+  }
+}
