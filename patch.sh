3b56dddc11b18c03fa92ec1dfe7b7d242e8e218c
Matteo C <hello@micorksen.eu>
2024-06-05 18:10:10 +0200

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ROOT_DIR="/home/springboard/springboard"
 VERSION="5.1.0"
-FILES=(
+SERVICES=(
   "Microsoft365"
 )
 
@@ -10,11 +10,16 @@ curl -so /tmp/opensaml.jar https://repo1.maven.org/maven2/org/opensaml/opensaml/
 curl -so /tmp/vertx-core.jar https://repo1.maven.org/maven2/io/vertx/vertx-core/3.9.5/vertx-core-3.9.5.jar
 echo "Downloaded 3 dependencies."
 
-for file in ${FILES[@]}; do
-  curl -so /tmp/$file.java https://raw.githubusercontent.com/acverne/auth-patch/main/services/$file.java?token=$(date +%s)
-  /usr/lib/jvm/temurin-8-jdk-amd64/bin/javac -Xlint:none -cp "$ROOT_DIR/mods/org.entcore~auth~$VERSION-fat.jar:/tmp/entcore-common.jar:/tmp/opensaml.jar:/tmp/vertx-core.jar" -d $ROOT_DIR/mods/org.entcore~auth~$VERSION/ /tmp/$file.java
-  echo "$file added."
-  rm /tmp/$file.java
+curl -so /tmp/SamlValidator.java https://raw.githubusercontent.com/acverne/auth-patch/main/security/SamlValidator.java?token=$(date +s)
+/usr/lib/jvm/temurin-8-jdk-amd64/bin/javac -Xlint:none -cp "$ROOT_DIR/mods/org.entcore~auth~$VERSION-fat.jar:/tmp/entcore-common.jar:/tmp/opensaml.jar:/tmp/vertx-core.jar" -d $ROOT_DIR/mods/org.entcore~auth~$VERSION/ /tmp/SamlValidator.java
+echo "Patched SamlValidator."
+rm /tmp/SamlValidator.java
+
+for service in ${SERVICES[@]}; do
+  curl -so /tmp/$service.java https://raw.githubusercontent.com/acverne/auth-patch/main/services/$service.java?token=$(date +%s)
+  /usr/lib/jvm/temurin-8-jdk-amd64/bin/javac -Xlint:none -cp "$ROOT_DIR/mods/org.entcore~auth~$VERSION-fat.jar:/tmp/entcore-common.jar:/tmp/opensaml.jar:/tmp/vertx-core.jar" -d $ROOT_DIR/mods/org.entcore~auth~$VERSION/ /tmp/$service.java
+  echo "$service service added."
+  rm /tmp/$service.java
 done
 
 rm /tmp/{entcore-common,opensaml,vertx-core}.jar
