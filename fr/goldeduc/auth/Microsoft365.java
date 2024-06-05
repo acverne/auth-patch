a0a277aafd175f086e22924de0fc63fd74685934
Matteo C <hello@micorksen.eu>
2024-06-05 14:07:36 +0200

    Update Microsoft365.java

--- fr/goldeduc/auth/Microsoft365.java
+++ fr/goldeduc/auth/Microsoft365.java
@@ -5,12 +5,16 @@ import io.vertx.core.Handler;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.entcore.auth.services.impl.AbstractSSOProvider;
 import org.entcore.common.neo4j.Neo4j;
 import org.entcore.common.neo4j.Neo4jResult;
 import org.opensaml.saml2.core.Assertion;
 
 public class Microsoft365 extends AbstractSSOProvider {
+  private static final Logger log = LoggerFactory.getLogger(Microsoft365.class);
+
   @Override
   public void execute(Assertion assertion, Handler<Either<String, Object>> handler) {
     handler.handle(new Either.Left<String, Object>("Execute function isn't available on this implementation."));
@@ -22,7 +26,7 @@ public class Microsoft365 extends AbstractSSOProvider {
       "-[:IN]->(:Group)-[:AUTHORIZED]->(:Role)-[:AUTHORIZE]->(:Action)<-[:PROVIDE]-(a:Application) " +
       "WHERE a.address = {serviceProviderEntityId} " +
       "RETURN DISTINCT u.email as email";
-    
+
     Neo4j.getInstance().execute(query, new JsonObject().put("userId", userId).put("serviceProviderEntityId", "https://login.microsoftonline.com/?whr=" + host.replace("ent.", "")), Neo4jResult.validUniqueResultHandler(evt -> {
       if (evt.isLeft()) {
         handler.handle(new Either.Left(evt.left().getValue()));
@@ -31,6 +35,8 @@ public class Microsoft365 extends AbstractSSOProvider {
 
       JsonArray result = new JsonArray();
       JsonObject user = evt.right().getValue();
+      log.info(user);
+
       result.add(new JsonObject().put("IDPEmail", user.getString("email", "")));
       handler.handle(new Either.Right<>(result));
     }));
