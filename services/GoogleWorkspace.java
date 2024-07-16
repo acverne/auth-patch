a4d934716fb0a612014b40f1e7cfd1778ee59d0a
Matteo C. <hello@micorksen.eu>
2024-07-16 15:06:08 +0200

    Update GoogleWorkspace.java

--- services/GoogleWorkspace.java
+++ services/GoogleWorkspace.java
@@ -22,7 +22,7 @@ public class GoogleWorkspace extends AbstractSSOProvider {
       "WHERE a.address STARTS WITH {serviceProviderEntityId} " +
       "RETURN DISTINCT u.email as email";
 
-    Neo4j.getInstance().execute(query, new JsonObject().put("userId", userId).put("serviceProviderEntityId", "https://www.google.com/a/" + host.replace("ent.", "") + "/ServiceLogin"), Neo4jResult.validUniqueResultHandler(evt -> {
+    Neo4j.getInstance().execute(query, new JsonObject().put("userId", userId).put("serviceProviderEntityId", "https://www." + serviceProviderEntityId + "/ServiceLogin"), Neo4jResult.validUniqueResultHandler(evt -> {
       if (evt.isLeft()) {
         handler.handle(new Either.Left(evt.left().getValue()));
         return;
